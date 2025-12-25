3e3eff35f0319841b8f2525cb209e600ceed4457
pedrosamegima <phsamegima@gmail.com>
2025-12-25 17:16:23 -0300

    Projeto Diflen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import Lenis from 'lenis';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Diflen';
+
+  ngOnInit(): void {
+    //Chamando o lenis
+    const lenis = new Lenis();
+
+    //função da animação que vai rodar a cada frame 
+    const raf = (time: number)=>{
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    };
+    requestAnimationFrame(raf)
+  }
 }
