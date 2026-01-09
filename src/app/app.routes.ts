1dec4499af7bb7320d49f38c5b5912c4e4faaff7
pedrosamegima <phsamegima@gmail.com>
2026-01-09 01:54:19 -0300

    Rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { HomeComponent } from './home/home.component';
 import { MinisteriosComponent } from './ministerios/ministerios.component';
 
 export const routes: Routes = [
-    {path:'', component: HomeComponent},
+    {path:'home', component: HomeComponent},
     {path:'ministerios', component: MinisteriosComponent}
 ];
