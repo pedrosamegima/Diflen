b820deb2f844c877034d87da957c856e4ef5538c
pedrosamegima <phsamegima@gmail.com>
2026-01-12 20:44:03 -0300

    Celulas

--- src/app/celulas/celulas.component.ts
+++ src/app/celulas/celulas.component.ts
@@ -8,7 +8,6 @@ export interface Celula{
   nome: string;
   lideres: string;
   diaHora: string;
-  descricao: string;
   imagem: string;
   telefone: string;
 }
@@ -23,10 +22,9 @@ export class CelulasComponent {
 
    celulas: Celula[] = [
     {
-      nome: 'LIFE DIFLEN - VILA NASTRI',
+      nome: 'LIFE DIFLEN - Athenas do Sul',
       lideres: 'Amanda e Gabriel',
       diaHora: 'Sexta-Feira ás 20hrs',
-      descricao: 'Um ambiente cheio de fé, amizade e crescimento espiritual para jovens.',
       imagem: '/celula3.jpeg',
       telefone: '5515991234567'
     },
@@ -34,15 +32,13 @@ export class CelulasComponent {
       nome: 'LIFE DIFLEN - LAGO DOS IPÉS',
       lideres: 'Maju e Octavio',
       diaHora: 'Quarta-Feira ás 20hrs',
-      descricao: 'Células voltadas para fortalecer famílias na presença de Deus.',
       imagem: '/assets/celula-familia.jpg',
       telefone: '5515987654321'
     },
     {
-      nome: 'LIFE DIFLEN - Jardim Fogaça',
+      nome: 'DIFLEN+ - Jardim Fogaça',
       lideres: 'Geh e Caf',
       diaHora: 'Quinta-Feira ás 20hrs',
-      descricao: 'Lugar onde as crianças aprendem a Palavra com alegria e amor.',
       imagem: '/assets/celula-kids.jpg',
       telefone: '5515977777777'
     },
@@ -50,7 +46,34 @@ export class CelulasComponent {
       nome: 'LIFE DIFLEN - Jardim Italia',
       lideres: 'Geh e Caf',
       diaHora: 'Quinta-Feira ás 20hrs',
-      descricao: 'Lugar onde as crianças aprendem a Palavra com alegria e amor.',
+      imagem: '/assets/celula-kids.jpg',
+      telefone: '5515977777777'
+    },
+    {
+      nome: 'LIFE DIFLEN - Jardim Shangrilla',
+      lideres: 'João e Natália',
+      diaHora: 'Sexta-Feira ás 20hrs',
+      imagem: '/assets/celula-kids.jpg',
+      telefone: '5515977777777'
+    },
+    {
+      nome: 'LIFE DIFLEN - Paineiras',
+      lideres: 'Tarik e Queren',
+      diaHora: 'Quinta-Feira ás 20hrs',
+      imagem: '/assets/celula-kids.jpg',
+      telefone: '5515977777777'
+    },
+    {
+      nome: 'DIFLEN+ - Jardim Alvorada',
+      lideres: 'Douglas e Júlia',
+      diaHora: 'Sexta-Feira ás 20hrs',
+      imagem: '/assets/celula-kids.jpg',
+      telefone: '5515977777777'
+    },
+     {
+      nome: 'Life Conexão - Online',
+      lideres: 'Rafael e Ana',
+      diaHora: 'Quarta-Feira ás 20hrs',
       imagem: '/assets/celula-kids.jpg',
       telefone: '5515977777777'
     },
