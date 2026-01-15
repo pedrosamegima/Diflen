84798211e5274c42cc474f5507ea7835d3ea52b2
pedrosamegima <phsamegima@gmail.com>
2026-01-15 16:41:17 -0300

    Novas atualizações

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './home/home.component';
 import { MinisteriosComponent } from './ministerios/ministerios.component';
 import { CelulasComponent } from './celulas/celulas.component';
 import { ProjetosComponent } from './projetos/projetos.component';
+import { DepoimentosComponent } from './depoimentos/depoimentos.component';
 
 export const routes: Routes = [
     {path:'home', component: HomeComponent},
     {path:'ministerios', component: MinisteriosComponent},
     {path:'celulas', component: CelulasComponent},
-    {path:'projetos', component: ProjetosComponent}
+    {path:'projetos', component: ProjetosComponent},
+     {path:'depoimentos', component: DepoimentosComponent}
 ];
